bd93761373c049b237c31d49815df42898cbceb7
Robin Bärtschi <rtmbaertschi007@gmail.com>
2025-09-12 20:40:56 +0200

    enhance lsp expirience

--- lua/config/lsp.fnl
+++ lua/config/lsp.fnl
@@ -1,39 +1,101 @@
 (local config (vim.fn.stdpath :config))
 
-(local group (vim.api.nvim_create_augroup "vimrc-lsp" { :clear true }))
+(fn client-supports-method [client method ?bufnr]
+  (if
+    (= (vim.fn.has :nvim-0.11) 1)
+    (client:supports_method method ?bufnr)
+    (client.supports_method method { :bufnr ?bufnr })
+    ))
+
 (vim.api.nvim_create_autocmd
   "LspAttach"
   {
-    : group
+    :group (vim.api.nvim_create_augroup "vimrc-lsp-attach" { :clear true })
     :callback
     (lambda [ev]
-      (print "hi")
-      (let [map (lambda [keys func desc] (vim.keymap.set "n" keys func { :buffer ev.buf :desc (.. "LSP: " desc) }))]
-        (map :gd (. (require :telescope.builtin) :lsp_definitions) "[G]oto [D]efinition")
-        (map :gr (. (require :telescope.builtin) :lsp_references) "[G]oto [R]eferences")
-        (map :gI (. (require :telescope.builtin) :lsp_implementations) "[G]oto [I]mplementation")
-        (map "<leader>D" (. (require "telescope.builtin") :lsp_type_definitions) "Type [D]efinition")
-        (map "<leader>ds" (. (require "telescope.builtin") :lsp_document_symbols) "[D]ocument [S]ymbols")
-        (map "<leader>ws" (. (require "telescope.builtin") :lsp_dynamic_workspace_symbols) "[W]orkspace [S]ymbols")
-        (map "<leader>rn" vim.lsp.buf.rename "[R]e[n]ame")
-        (map "<leader>ca" vim.lsp.buf.code_action "[C]ode [A]ction")
-        (map "gD" vim.lsp.buf.declaration "[G]oto [D]eclaration")
-        )
-      (let [client (assert (vim.lsp.get_client_by_id ev.data.client_id))]
-        (when (and
-                (not (client:supports_method "textDocument/willSaveWaitUntil"))
-                (client:supports_method "textDocument/formatting"))
-          (vim.api.nvim_create_autocmd
-            "BufWritePre"
-            {
-            : group
-            :buffer ev.buf
-            :callback
-            (lambda [ev]
-              (vim.lsp.buf.format { :bufnr ev.buf :id client.id :timeout_ms 1000 }))
-            }))))
-  }
-  )
+      (let [map (fn map [keys func desc ?mode] (vim.keymap.set (or ?mode "n") keys func { :buffer ev.buf :desc (.. "LSP: " desc) }))]
+        (map :grn vim.lsp.buf.rename "[R]e[n]ame")
+        (map :gra vim.lsp.buf.code_action "[C]ode [A]ction" [ :n :x ])
+        (map :grr (. (require :telescope.builtin) :lsp_references) "[G]oto [R]eferences")
+        (map :gri (. (require :telescope.builtin) :lsp_implementations) "[G]oto [I]mplementation")
+        (map :grd (. (require :telescope.builtin) :lsp_definitions) "[G]oto [D]efinition")
+        (map :grD vim.lsp.buf.declaration "[G]oto [D]eclaration")
+        (map :gO (. (require "telescope.builtin") :lsp_document_symbols) "[D]ocument [S]ymbols")
+        (map :gW (. (require "telescope.builtin") :lsp_dynamic_workspace_symbols) "[W]orkspace [S]ymbols")
+        (map :grt (. (require "telescope.builtin") :lsp_type_definitions) "Type [D]efinition")
+        (let [client (vim.lsp.get_client_by_id ev.data.client_id)]
+         (when (and client (client-supports-method client vim.lsp.protocol.Methods.textDocument_documentHighlight ev.buf))
+           (let [highlight_augroup (vim.api.nvim_create_augroup :vimrc-lsp-highlight { :clear false })]
+             (vim.api.nvim_create_autocmd
+               [:CursorHold :CursorHoldI]
+               {
+               :buffer ev.buf
+               :group highlight_augroup
+               :callback vim.lsp.buf.document_highlight
+               })
+             (vim.api.nvim_create_autocmd
+               [:CursorMoved :CursorMovedI]
+               {
+               :buffer ev.buf
+               :group highlight_augroup
+               :callback vim.lsp.buf.clear_references
+               })
+             (vim.api.nvim_create_autocmd
+               :LspDetach
+               {
+               :group (vim.api.nvim_create_augroup :vimrc-lsp-detach { :clear true })
+               :callback (lambda [ev] (vim.lsp.buf.clear_references) (vim.api.nvim_clear_autocmds { :group :vimrc-lsp-highlight :buffer ev2.buf }))
+               })
+             ))
+         (when (and client (client-supports-method client vim.lsp.protocol.Methods.textDocument_inlayHint ev.buf))
+           (map
+             "<leader>th"
+             (lambda []
+               (vim.lsp.inlay_hint.enable (not (vim.lsp.inlay_hint.is_enabled { :bufnr ev.buf }))))
+             "[T]oggle Inlay [H]ints"
+             ))
+         (when (and client (and
+                 (not (client-supports-method client "textDocument/willSaveWaitUntil"))
+                 (client-supports-method client "textDocument/formatting")))
+           (vim.api.nvim_create_autocmd
+             "BufWritePre"
+             {
+             : group
+             :buffer ev.buf
+             :callback
+             (lambda [ev]
+               (vim.lsp.buf.format { :bufnr ev.buf :id client.id :timeout_ms 1000 }))
+             }))
+         (vim.diagnostic.config
+           {
+           :severity_sort true
+           :float { :border :rounded :source :if_many }
+           :underline { :severity vim.diagnostic.severity.ERROR }
+           :signs (or (and vim.g.have_nerd_font
+                           {
+                           :text {
+                           vim.diagnostic.severity.ERROR "󰅚 "
+                           vim.diagnostic.severity.WARN "󰀪 "
+                           vim.diagnostic.severity.INFO "󰋽 "
+                           vim.diagnostic.severity.HINT "󰌶 "
+                           }
+                           })
+                      {})
+           :virtual_text {
+           :source :if_many
+           :spacing 2
+           :format (lambda [diagnostic]
+                     (. {
+                     vim.diagnostic.severity.ERROR diagnostic.message
+                     vim.diagnostic.severity.WARN diagnostic.message
+                     vim.diagnostic.severity.INFO diagnostic.message
+                     vim.diagnostic.severity.HINT diagnostic.message
+                     } diagnostic.severity)
+                     )
+           }
+           })
+
+         )) nil)})
 
 
 (local lsps ["lua_ls" "rust_analyzer" "ts_ls"])
--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,39 +1,65 @@
 local config = vim.fn.stdpath("config")
-local group = vim.api.nvim_create_augroup("vimrc-lsp", {clear = true})
-local function _1_(ev)
-  _G.assert((nil ~= ev), "Missing argument ev on C:\\Users\\rtmba\\AppData\\Local\\nvim\\lua\\config\\lsp.fnl:9")
-  print("hi")
-  do
-    local map
-    local function _2_(keys, func, desc)
-      _G.assert((nil ~= desc), "Missing argument desc on C:\\Users\\rtmba\\AppData\\Local\\nvim\\lua\\config\\lsp.fnl:11")
-      _G.assert((nil ~= func), "Missing argument func on C:\\Users\\rtmba\\AppData\\Local\\nvim\\lua\\config\\lsp.fnl:11")
-      _G.assert((nil ~= keys), "Missing argument keys on C:\\Users\\rtmba\\AppData\\Local\\nvim\\lua\\config\\lsp.fnl:11")
-      return vim.keymap.set("n", keys, func, {buffer = ev.buf, desc = ("LSP: " .. desc)})
-    end
-    map = _2_
-    map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-    map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-    map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-    map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
-    map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-    map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
-    map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-    map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-    map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-  end
-  local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
-  if (not client:supports_method("textDocument/willSaveWaitUntil") and client:supports_method("textDocument/formatting")) then
-    local function _3_(ev0)
-      _G.assert((nil ~= ev0), "Missing argument ev on C:\\Users\\rtmba\\AppData\\Local\\nvim\\lua\\config\\lsp.fnl:32")
-      return vim.lsp.buf.format({bufnr = ev0.buf, id = client.id, timeout_ms = 1000})
-    end
-    return vim.api.nvim_create_autocmd("BufWritePre", {group = group, buffer = ev.buf, callback = _3_})
+local function client_supports_method(client, method, _3fbufnr)
+  if (vim.fn.has("nvim-0.11") == 1) then
+    return client:supports_method(method, _3fbufnr)
   else
-    return nil
+    return client.supports_method(method, {bufnr = _3fbufnr})
   end
 end
-vim.api.nvim_create_autocmd("LspAttach", {group = group, callback = _1_})
+local function _2_(ev)
+  _G.assert((nil ~= ev), "Missing argument ev on C:\\Users\\Robin\\AppData\\Local\\nvim\\lua\\config\\lsp.fnl:15")
+  do
+    local map
+    local function map0(keys, func, desc, _3fmode)
+      return vim.keymap.set((_3fmode or "n"), keys, func, {buffer = ev.buf, desc = ("LSP: " .. desc)})
+    end
+    map = map0
+    map("grn", vim.lsp.buf.rename, "[R]e[n]ame")
+    map("gra", vim.lsp.buf.code_action, "[C]ode [A]ction", {"n", "x"})
+    map("grr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+    map("gri", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+    map("grd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+    map("grD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+    map("gO", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+    map("gW", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+    map("grt", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if (client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, ev.buf)) then
+      local highlight_augroup = vim.api.nvim_create_augroup("vimrc-lsp-highlight", {clear = false})
+      vim.api.nvim_create_autocmd({"CursorHold", "CursorHoldI"}, {buffer = ev.buf, group = highlight_augroup, callback = vim.lsp.buf.document_highlight})
+      vim.api.nvim_create_autocmd({"CursorMoved", "CursorMovedI"}, {buffer = ev.buf, group = highlight_augroup, callback = vim.lsp.buf.clear_references})
+      local function _3_(ev0)
+        _G.assert((nil ~= ev0), "Missing argument ev on C:\\Users\\Robin\\AppData\\Local\\nvim\\lua\\config\\lsp.fnl:47")
+        vim.lsp.buf.clear_references()
+        return vim.api.nvim_clear_autocmds({group = "vimrc-lsp-highlight", buffer = ev2.buf})
+      end
+      vim.api.nvim_create_autocmd("LspDetach", {group = vim.api.nvim_create_augroup("vimrc-lsp-detach", {clear = true}), callback = _3_})
+    else
+    end
+    if (client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, ev.buf)) then
+      local function _5_()
+        return vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({bufnr = ev.buf}))
+      end
+      map("<leader>th", _5_, "[T]oggle Inlay [H]ints")
+    else
+    end
+    if (client and (not client_supports_method(client, "textDocument/willSaveWaitUntil") and client_supports_method(client, "textDocument/formatting"))) then
+      local function _7_(ev0)
+        _G.assert((nil ~= ev0), "Missing argument ev on C:\\Users\\Robin\\AppData\\Local\\nvim\\lua\\config\\lsp.fnl:66")
+        return vim.lsp.buf.format({bufnr = ev0.buf, id = client.id, timeout_ms = 1000})
+      end
+      vim.api.nvim_create_autocmd("BufWritePre", {group = group, buffer = ev.buf, callback = _7_})
+    else
+    end
+    local function _9_(diagnostic)
+      _G.assert((nil ~= diagnostic), "Missing argument diagnostic on C:\\Users\\Robin\\AppData\\Local\\nvim\\lua\\config\\lsp.fnl:87")
+      return ({[vim.diagnostic.severity.ERROR] = diagnostic.message, [vim.diagnostic.severity.WARN] = diagnostic.message, [vim.diagnostic.severity.INFO] = diagnostic.message, [vim.diagnostic.severity.HINT] = diagnostic.message})[diagnostic.severity]
+    end
+    vim.diagnostic.config({severity_sort = true, float = {border = "rounded", source = "if_many"}, underline = {severity = vim.diagnostic.severity.ERROR}, signs = ((vim.g.have_nerd_font and {text = {[vim.diagnostic.severity.ERROR] = "\243\176\133\154 ", [vim.diagnostic.severity.WARN] = "\243\176\128\170 ", [vim.diagnostic.severity.INFO] = "\243\176\139\189 ", [vim.diagnostic.severity.HINT] = "\243\176\140\182 "}}) or {}), virtual_text = {source = "if_many", spacing = 2, format = _9_}})
+  end
+  return nil
+end
+vim.api.nvim_create_autocmd("LspAttach", {group = vim.api.nvim_create_augroup("vimrc-lsp-attach", {clear = true}), callback = _2_})
 local lsps = {"lua_ls", "rust_analyzer", "ts_ls"}
 for _, lsp in ipairs(lsps) do
   vim.lsp.enable(lsp)
